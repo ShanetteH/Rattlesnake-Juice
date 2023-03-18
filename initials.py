23f05ddc852ffab3690b654978a4501214026e6b
Shanette <shanette.h7@gmail.com>
2023-03-17 22:33:05 -0400

    completed some exercises

---
+++ initials.py
@@ -0,0 +1,7 @@
+# This is a comment UWU
+
+print('     SSSSSS     H     H')
+print('    S           H     H')
+print('     SSSSS      HHHHHHH')
+print('          S     H     H')
+print('    SSSSSS      H     H')
