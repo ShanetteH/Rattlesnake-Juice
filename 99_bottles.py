2c8b82b7086d28ecd5311acc4cfe1f490cf637b2
Shanette <shanette.h7@gmail.com>
2023-04-01 20:51:48 -0400

    more math

---
+++ 99_bottles.py
@@ -0,0 +1,5 @@
+for i in range (99):
+    bottles = 99 - i
+    print (f"{bottles} bottles of beer on the wall, {bottles} bottles of beer")
+    print (f"Take one down, pass it around")
+    print (f"{bottles - 1} bottles of beer on the wall")
